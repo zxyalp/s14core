e63dd29b59a945df87ded2918097f0fe05bfd9b6
xing yan <zgyan012@gmail.com>
2018-03-02 18:09:19 +0800

    add file requirements.txt

--- day04/demo.py
+++ day04/demo.py
@@ -5,3 +5,32 @@ Created on 2017/7/3
 @author: summe
 """
 
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+
+    def setUp(self):
+        print("start;")
+
+    def test_add01(self):
+        print("test1")
+
+    def test_add02(self):
+        print("test2")
+
+    def tearDown(self):
+        print("end....")
+
+
+if __name__ == '__main__':
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestDemo('test_add01'))
+
+    runner = unittest.TextTestRunner()
+
+    runner.run(suite)
+
+

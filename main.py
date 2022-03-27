ea8c8812feb734c28398d606bba83784f9365a97
zxyalp <orcexzy@163.com>
2022-03-28 01:24:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Created on 2022/3/27 0027 18:31
+
+@author: zhou.yang
+"""
+from rich.console import Console
+console = Console()
+
+console.print("Hello", "World!")
+console.print("Hello", "World!", style="bold red")
